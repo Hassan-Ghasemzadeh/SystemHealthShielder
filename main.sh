6bff6f10f2eb6ff271763eb887a170e5be1f5df2
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-25 12:42:34 +0330

    [Feat] : added imports in main.sh

--- main.sh
+++ main.sh
@@ -3,3 +3,7 @@
 # Safty standard for bash scripts
 set -euo pipefail
 IFS=$'\n\t'
+
+readonly APP_ROOT=$(dirname "$(readlink -f "$0")")
+source "$APP_ROOT/config/app.conf"
+source "$APP_ROOT/lib/utils.sh"
