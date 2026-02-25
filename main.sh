1198d5cc1fc0fd1ae6d627acb946c2e12a418bab
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-25 12:45:09 +0330

    [Feat] : add trap for cleanup function to stop the script when an error happen

--- main.sh
+++ main.sh
@@ -7,3 +7,9 @@ IFS=$'\n\t'
 readonly APP_ROOT=$(dirname "$(readlink -f "$0")")
 source "$APP_ROOT/config/app.conf"
 source "$APP_ROOT/lib/utils.sh"
+
+cleanup(){
+    log_event "INFO" "Securely closing script..."
+    exit 1
+}
+trap cleanup EXIT
