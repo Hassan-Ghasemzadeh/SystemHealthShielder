dbfb115c72465d491908bac7fd4e0ef89ed200d3
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-25 12:27:13 +0330

    [Feat] : log message function created

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,1 +1,21 @@
 #!/usr/bin/env bash
+
+log_event(){
+    local level=$1
+    local message=$2
+    local timestamp=$(date +%Y-%m-%d %H-%M-%S)
+
+    mkdir -p "$(dirname "$LOG_FILE")"
+
+    case $level in
+        "INFO")
+            echo -e "[$timestamp] [\e[32mINFO\e[0m] $message" | tee -a "LOG_FILE"
+            ;;
+        "WARN")
+            echo -e "[$timestamp] [\e[33mWARN\e[0m] $message" | tee -a "LOG_FILE"
+            ;;
+        "ERROR")
+            echo -e "[$timestamp] [\e[31mERROR\e[0m] $message" | tee -a "LOG_FILE"
+            ;;
+    esac
+}
