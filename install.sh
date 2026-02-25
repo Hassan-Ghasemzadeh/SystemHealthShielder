a77dc0d87eff3f1753ef85ba92dc3d3bb54f046b
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-25 14:06:45 +0330

    [Feat] : check root function added

--- install.sh
+++ install.sh
@@ -2,3 +2,9 @@
 
 INSTALL_DIR="/opt/system-health-shielder"
 BIN_LINK="/usr/local/bin/system-health-shielder"
+
+# Check if the script is run as root
+if [[ "$EUID" -ne 0 ]]; then
+    echo "Please run as root"
+    exit 1
+fi
