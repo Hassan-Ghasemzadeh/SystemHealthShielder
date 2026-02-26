f5497e1987664903a5d90598f136eb9bd65ee8e0
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-26 17:06:01 +0330

    [Fix] : explanation added

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 log_event(){
     local level=$1
     local message=$2
@@ -8,17 +9,21 @@ log_event(){
     mkdir -p "$(dirname "$LOG_FILE")"
 
     case $level in
-        "INFO")
+        "INFO")\
+            #green
             echo -e "[$timestamp] [\e[32mINFO\e[0m] $message" | tee -a "LOG_FILE"
             ;;
         "WARN")
+            #yellow
             echo -e "[$timestamp] [\e[33mWARN\e[0m] $message" | tee -a "LOG_FILE"
             ;;
         "ERROR")
+            #red
             echo -e "[$timestamp] [\e[31mERROR\e[0m] $message" | tee -a "LOG_FILE"
             ;;
     esac
 }
+
 
 check_root(){
     if [[ $EUID -ne 0 ]]
