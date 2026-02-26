50a33b87dd7cbcf8b63d58dd3e0ff8fde1da075a
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-26 17:06:49 +0330

    [Fix] : explanation added

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 INSTALL_DIR="/opt/system-health-shielder"
 BIN_LINK="/usr/local/bin/system-health-shielder"
 
@@ -15,9 +16,12 @@ mkdir -p "$INSTALL_DIR"
 # Copy files to the installation directory
 cp -r ./* "$INSTALL_DIR"
 
+# Make the main script executable
 chmod +x "$INSTALL_DIR/main.sh"
 
+# Create a symbolic link to the main script in /usr/local/bin
 ln -sf "$INSTALL_DIR/main.sh" "$BIN_LINK"
+
 
 echo "Installation completed successfully!"
 echo "You can run the tool using: system-health-shielder"
