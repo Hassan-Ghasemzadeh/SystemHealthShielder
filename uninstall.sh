05a2c578d82e1aad4d8e92afe6fccee4f80642bd
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-26 17:03:23 +0330

    [Fix]

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+
 INSTALL_DIR="/opt/system-health-shielder"
 BIN_LINK="/usr/local/bin/system-health-shielder"
+
 
 # Check if the script is run as root
 if [[ "$EUID" -ne 0 ]]; then
@@ -9,12 +11,16 @@ if [[ "$EUID" -ne 0 ]]; then
     exit 1
 fi
 
+
 echo "Uninstalling System Health Shielder..."
+
 
 # Remove the binary link
 rm -f "$BIN_LINK"
+
 
 # Remove the installation directory
 rm -rf "$INSTALL_DIR"
 
 echo "Uninstallation completed successfully!"
+
